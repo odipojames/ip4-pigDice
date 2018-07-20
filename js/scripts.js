7809e1f5e0abd768575f946256bce911e46f1189
odipojames <odipojames12@gmai.com>
2018-07-20 12:08:50 +0300

    add rolling buttons

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,12 @@ function rollDice(){
   if (d1==1 && d2==1){
     status.innerHTML-=status.innerHTML+0;// "your total is 0"+"its player2 free turn!!";
   }
+  else if (d1==d2){
+    status.innerHTML+= "Doubles!!You get a free turn!";
+
+  }
   else {
     status.innerHTML;
-
   }
 }
 
@@ -24,8 +27,8 @@ function rollDice2(){
   var die2 = document.getElementById('die2');
   var status=document.getElementById('status');
   var status2=document.getElementById('status2');
-  var d1 = Math.floor(Math.random()*6)+1;
-  var d2 = Math.floor(Math.random()*6)+1;
+  var d1 = Math.floor(Math.random()*100)+1;
+  var d2 = Math.floor(Math.random()*100)+1;
   var diceTotal = d1+d2;
   die1.innerHTML = d1;
   die2.innerHTML = d2;
@@ -34,4 +37,13 @@ function rollDice2(){
 
     status2.innerHTML -=status2.innerHTML+0//" its player1 turn!!";
   }
+  else if(d2==d1){
+    status2.innerHTML+= "Doubles!!You get a free turn!";
+  }
+
+  else {
+    status2.innerHTML;
+
+  }
+
 }
